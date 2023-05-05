bad1e041daed971c0a0a7c9ef33584a463357579
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-05-06 03:20:58 +0545

    Add app.py

--- app/app.py
+++ app/app.py
@@ -5,16 +5,16 @@ from loguru import logger
 logger.info("Loading .env file")
 load_dotenv()
 
-from .controllers.riddle import riddle_blueprint
-from .controllers.users import user_blueprint
+from .controllers.hunt import hunt_blueprint
+from .controllers.treasure import treasure_blueprint
 
 app = Flask(__name__)
 
 URL_PREFIX = "/hunt"
 
 # Blueprints
-app.register_blueprint(user_blueprint, url_prefix=URL_PREFIX)
-app.register_blueprint(riddle_blueprint, url_prefix=URL_PREFIX)
+app.register_blueprint(treasure_blueprint, url_prefix=URL_PREFIX)
+app.register_blueprint(hunt_blueprint, url_prefix=URL_PREFIX)
 
 if __name__ == "__main__":
     app.run()

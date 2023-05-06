98b08a478b66126c7c9eb6af6d827509494a321e
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-05-06 13:39:36 +0545

    Update bind ip

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn --workers 1 -k gthread --thread=8 --timeout 900 --name treasurehunt --reload app.app:app
+gunicorn --workers 1 -k gthread --thread=8 --timeout 900 --name treasurehunt -b 0.0.0.0:8000 --reload app.app:app
